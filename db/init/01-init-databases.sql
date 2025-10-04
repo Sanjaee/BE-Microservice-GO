4cd9486398516ae241385b2673dbe30ef0c2b470
Sanjaee <afrizaahmad18@gmail.com>
2025-10-04 09:26:40 +0700

    test

--- db/init/01-init-databases.sql
+++ db/init/01-init-databases.sql
@@ -1,4 +1,10 @@
 -- Buat DB tambahan untuk microservices
-CREATE DATABASE userdb;
-CREATE DATABASE productdb;
-CREATE DATABASE paymentdb;
+-- userdb sudah dibuat sebagai default database di docker-compose.yml
+
+-- Buat database productdb jika belum ada
+SELECT 'CREATE DATABASE productdb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'productdb')\gexec
+
+-- Buat database paymentdb jika belum ada  
+SELECT 'CREATE DATABASE paymentdb'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'paymentdb')\gexec
